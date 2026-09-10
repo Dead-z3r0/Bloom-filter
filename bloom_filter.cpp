d8182c9b460fc2bc87b84d30601c6eba77c8ca76
Dead-z3r0 <ap7458725@gmail.com>
2026-09-11 00:46:03 +0530

    adding constructor, add and taken function

--- bloom_filter.cpp
+++ bloom_filter.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 #include<chrono>
 
+
 using namespace std;
 
 class bloom_filter{
@@ -17,6 +18,31 @@ class bloom_filter{
                 hash_value=(hash_value*seed+(int)(inpt[i]))%size_bft;
             }
             return hash_value%size_bft;
+        }
+    public :
+        bloom_filter(int size_bft, int hash_count){
+            this->size_bft=size_bft;
+            this->hash_count=hash_count;
+            bft.resize(size_bft,0); // initializing the bloom filter bit vector with 0
+        }
+
+        void add(string &inpt){
+            for(int i=0;i<hash_count;i++){
+                int hash_value=hash_of_i(inpt,i); // this call will return the hash value of the input string for the given seed value
+                bft[hash_value]=1; //set the hash value index in the bloom filter bit vector to 1
+            }
+            return;
+        }
+
+
+        bool taken(string &inpt){
+            for(int i=0;i<hash_count;i++){
+                int hash_value=hash_of_i(inpt,i); //return the hash value of the input string for the given seed value
+                if(!bft[hash_value]){ //if any hash value index is 0 it implies that the input string is DEF NOT PRESENT in the bloom filter
+                    return false;
+                }
+            }
+            return true; //The string is either PRESENT or FALSE POSITIVE in the bloom filter
         }
 };
 
